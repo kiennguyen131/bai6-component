40dfb277597855fb0803fd25d496207842a8514f
kien <sayohyeah17@gmail.com>
2020-05-28 10:22:18 +0700

    done bai 12-2

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,32 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./../css/SearchBox.css";
 
-class SearchBox extends Component{
-    constructor() {
-        super();
-        this.state = {
-          isFocused: false
-        };
-      }
-      render() {
-        return (
-          <div className="SearchBox">
+class SearchBox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isFocus: false,
+      lengthInput: 0
+    };
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
+  }
 
-              <input
-                onFocus={() => this.setState({ isFocused: true })}
-                onBlur={() => this.setState({ isFocused: false })}
-                type="text"
-                placeholder="Type something to search..."
-              />
-              {!this.state.isFocused && (
-                <button>
-                  <img src="https://cdn.glitch.com/44747202-fcc0-49dd-b4d1-e9c208ad632f%2Fmagnifying-glass.png?v=1588755021698" />
-                </button>
-              )}
+  onFocus() {
+    this.setState({ isFocus: true });
+  }
 
-          </div>
-        );
-      }
-    }
+  onBlur() {
+    this.setState({ isFocus: false });
+  }
+
+  onKeyUp() {
+    this.setState({
+      lengthInput: document.getElementById("searchInput").value.length
+    });
+  }
+
+  render() {
+    let { isFocus } = this.state;
+    let { lengthInput } = this.state;
+    const classNames = require("classnames");
+    const classNameOfSearchInput = classNames({
+      "out-length": lengthInput > 10
+    })
+    const className = classNames("search-btn", {
+      active: isFocus
+    });
+    return (
+      <div className="SearchBox">
+        <input
+          id="searchInput"
+          className={classNameOfSearchInput}
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          onKeyUp={this.onKeyUp}
+          type="text"
+          placeholder="Type something to search..."
+        />
+        <div className={className}>
+          <a href="#">
+            <img
+              src="https://image.flaticon.com/icons/svg/483/483356.svg"
+              width="15"
+            />
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default SearchBox;
